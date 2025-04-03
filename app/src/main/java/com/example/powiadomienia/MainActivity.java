67bf8d7b44d5e7833157379aa74bdc80a7ff3ad0
DawidB7 <podlasieauuau@gmail.com>
2025-04-03 15:13:37 +0200

    03.04.25

---
+++ app/src/main/java/com/example/powiadomienia/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.powiadomienia;
+
+import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.graphics.BitmapCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String CHANNEL_ID = "my_channel_id";
+    private static int ID = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+    }
+}
